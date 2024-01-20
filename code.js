d799d46a75988d8b298a534b289e74c54e2e98de
28kandala <113399868+28kandala@users.noreply.github.com>
2024-01-20 11:41:27 -0500

    Add files via upload

---
+++ code.js
@@ -0,0 +1,153 @@
+onEvent("Play", "click", function( ) {
+  setScreen("Play-app");
+});
+onEvent("Videos", "click", function( ) {
+  setScreen("Videos-");
+});
+onEvent("button16", "click", function( ) {
+  setScreen("HomeScreen");
+});
+onEvent("button17", "click", function( ) {
+  setScreen("HomeScreen");
+});
+onEvent("Soccer", "click", function( ) {
+  setScreen("soccer-videos");
+});
+onEvent("Basketball", "click", function( ) {
+  setScreen("Basketball-Videos");
+});
+onEvent("Hockey", "click", function( ) {
+  setScreen("Hockey-Videos");
+});
+onEvent("Dance", "click", function( ) {
+  setScreen("Dance-Videos");
+});
+onEvent("football", "click", function( ) {
+  setScreen("Football-Videos");
+});
+onEvent("lacrosse", "click", function( ) {
+  setScreen("Lacrosse-Videos");
+});
+onEvent("fencing", "click", function( ) {
+  setScreen("Fencing-Videos");
+});
+onEvent("baseball", "click", function( ) {
+  setScreen("Baseball-Videos");
+});
+onEvent("tennis", "click", function( ) {
+  setScreen("Tennis-Videos");
+});
+onEvent("volleyball", "click", function( ) {
+  setScreen("Volleyball-Videos");
+});
+onEvent("Soccerrules", "click", function( ) {
+  open("https://www.soccer.com/guide/rules-of-soccer-guide");
+});
+onEvent("soccervideo", "click", function( ) {
+  open("https://www.youtube.com/watch?v=IEluFKIoChI");
+});
+onEvent("Basketballrules", "click", function( ) {
+  open("https://www.masterclass.com/articles/basketball-rules-explained");
+});
+onEvent("BasketballVideo", "click", function( ) {
+  open("https://www.youtube.com/watch?v=wackmyGU734&t=132s");
+});
+onEvent("Hockeyrules", "click", function( ) {
+  open("https://www.owayo.com/magazine/rules-of-hockey-made-simple-us.htm");
+});
+onEvent("HockeyVideo", "click", function( ) {
+  open("https://www.youtube.com/watch?v=H_70vAiyyXM");
+});
+onEvent("Dancerules", "click", function( ) {
+  open("https://www.redbull.com/in-en/how-to-learn-dance-at-home");
+});
+onEvent("Dancevideo", "click", function( ) {
+  open("");
+});
+onEvent("BackButton-soccer", "click", function( ) {
+  setScreen("Videos-");
+});
+onEvent("BackButton-Baseball", "click", function( ) {
+  setScreen("Videos-");
+});
+onEvent("BackButton-Basketball", "click", function( ) {
+  setScreen("Videos-");
+});
+onEvent("BackButton-Dance", "click", function( ) {
+  setScreen("Videos-");
+});
+onEvent("BackButton-Fencing", "click", function( ) {
+  setScreen("Videos-");
+});
+onEvent("BackButton-Tennis", "click", function( ) {
+  setScreen("Videos-");
+});
+onEvent("BackButton-football", "click", function( ) {
+  setScreen("Videos-");
+});
+onEvent("BackButton-hockey", "click", function( ) {
+  setScreen("Videos-");
+});
+onEvent("BackButton-lacrosse", "click", function( ) {
+  setScreen("Videos-");
+});
+onEvent("FootballRules", "click", function( ) {
+  open("https://footballadvantage.com/football-rules/");
+});
+onEvent("FootballVideo", "click", function( ) {
+  open("https://www.youtube.com/watch?v=3t6hM5tRlfA");
+});
+onEvent("VolleyballRules", "click", function( ) {
+  open("https://usavolleyball.org/play/rules-of-volleyball/");
+});
+onEvent("VolleyballVideo", "click", function( ) {
+  open("https://www.youtube.com/watch?v=9g7nYQv-kPM");
+});
+onEvent("TennisRules", "click", function( ) {
+  open("https://supertennisracquet.com/tennis-rules/");
+});
+onEvent("TennisVideo", "click", function( ) {
+  open("https://www.youtube.com/watch?v=jrhM3k84YJU");
+});
+onEvent("LacrosseVideo", "click", function( ) {
+  open("https://www.youtube.com/watch?v=ZPbfK5ZJ9kQ");
+});
+onEvent("LacrosseRules", "click", function( ) {
+  open("https://www.lacrosserunner.com/lacrosse-rules/");
+});
+onEvent("BaseballRules", "click", function( ) {
+  open("https://probaseballguide.com/basic-rules-of-baseball-game/");
+});
+onEvent("BaseballVideo", "click", function( ) {
+  open("https://www.youtube.com/watch?v=XVFvu9beC7w");
+});
+onEvent("FencingRules", "click", function( ) {
+  open("https://www.rulesofsport.com/sports/fencing.html");
+});
+onEvent("FencingVideo", "click", function( ) {
+  open("https://www.youtube.com/watch?v=x7zxpDW8nb0");
+});
+onEvent("BackButton-Volleyball", "click", function( ) {
+  setScreen("Videos-");
+});
+onEvent("Purchase", "click", function( ) {
+  setScreen("purchase");
+});
+onEvent("buy", "click", function( ) {
+  setScreen("Order");
+});
+onEvent("button2", "click", function( ) {
+  setScreen("Orderform");
+});
+onEvent("purchasehome", "click", function( ) {
+  setScreen("HomeScreen");
+});
+onEvent("Orderback", "click", function( ) {
+  setScreen("purchase");
+});
+onEvent("Orderformhome", "click", function( ) {
+  setScreen("HomeScreen");
+});
+onEvent("Orderformback", "click", function( ) {
+  setScreen("Order");
+});
